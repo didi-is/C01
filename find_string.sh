0ba54cbeb86ed39d9f81fb83f3bb8f5e9b5eb228
didi-is <ih4683448@gmail.com>
2025-05-21 22:26:05 +0100

    mise a jour de find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Nom du fichier dans lequel effectuer la recherche
 read f
+
 # chaine de caractere a rechercher
 read c
+
 # verifier si la chaine a ete trouver
 if grep -q "$c" "$f"; then
 echo "La chaine '$c' a été trouvée dans $f."
